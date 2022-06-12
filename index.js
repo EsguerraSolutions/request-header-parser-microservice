f6f48555a04808ab2541513dd5dae41986730fc0
Jonathan Esguerra <EsguerraDesign@gmail.com>
2022-06-12 13:30:31 +0800

    edited whoami endpoint

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+//API endpoint for request header parser
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    ipaddress : req.ip
+  });
+});
+
 
 
 // listen for requests :)
